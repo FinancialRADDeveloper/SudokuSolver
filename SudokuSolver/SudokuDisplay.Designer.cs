19ab012b4ace61fe0ec0f4413b887a974ceaf74a
russalan <alan_russell@hotmail.com>
2016-03-08 23:25:42 +0000

    Add further Files

---
+++ SudokuSolver/SudokuDisplay.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace SudokuSolver
+{
+    partial class SudokuDisplay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SudokuGridView = new System.Windows.Forms.DataGridView();
+            this.txtLogDisplay = new System.Windows.Forms.RichTextBox();
+            this.PreSuppliedGridView = new System.Windows.Forms.DataGridView();
+            this.FileNameLbl = new System.Windows.Forms.Label();
+            this.SudokuStatusStrip = new System.Windows.Forms.StatusStrip();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFileNameDisplay = new System.Windows.Forms.TextBox();
+            this.lblLogInfo = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lblTileTitle = new System.Windows.Forms.Label();
+            this.btnGetTileForCell = new System.Windows.Forms.Button();
+            this.TileGridView = new System.Windows.Forms.DataGridView();
+            this.lblLowestValue = new System.Windows.Forms.Label();
+            this.txtLowestInArray = new System.Windows.Forms.TextBox();
+            this.btnGetLowestValue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.SudokuGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PreSuppliedGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TileGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SudokuGridView
+            // 
+            this.SudokuGridView.AllowUserToAddRows = false;
+            this.SudokuGridView.AllowUserToDeleteRows = false;
+            this.SudokuGridView.AllowUserToResizeRows = false;
+            this.SudokuGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.SudokuGridView.BackgroundColor = System.Drawing.Color.Beige;
+            this.SudokuGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SudokuGridView.ColumnHeadersVisible = false;
+            this.SudokuGridView.Location = new System.Drawing.Point(12, 12);
+            this.SudokuGridView.Name = "SudokuGridView";
+            this.SudokuGridView.RowHeadersVisible = false;
+            this.SudokuGridView.RowHeadersWidth = 18;
+            this.SudokuGridView.Size = new System.Drawing.Size(272, 251);
+            this.SudokuGridView.TabIndex = 0;
+            this.SudokuGridView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.SudokuGridView_CellValueChanged);
+            // 
+            // txtLogDisplay
+            // 
+            this.txtLogDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLogDisplay.Location = new System.Drawing.Point(12, 306);
+            this.txtLogDisplay.Name = "txtLogDisplay";
+            this.txtLogDisplay.Size = new System.Drawing.Size(272, 69);
+            this.txtLogDisplay.TabIndex = 1;
+            this.txtLogDisplay.Text = "";
+            // 
+            // PreSuppliedGridView
+            // 
+            this.PreSuppliedGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PreSuppliedGridView.Location = new System.Drawing.Point(322, 11);
+            this.PreSuppliedGridView.Name = "PreSuppliedGridView";
+            this.PreSuppliedGridView.Size = new System.Drawing.Size(291, 40);
+            this.PreSuppliedGridView.TabIndex = 2;
+            // 
+            // FileNameLbl
+            // 
+            this.FileNameLbl.AutoSize = true;
+            this.FileNameLbl.Location = new System.Drawing.Point(319, 277);
+            this.FileNameLbl.Name = "FileNameLbl";
+            this.FileNameLbl.Size = new System.Drawing.Size(54, 13);
+            this.FileNameLbl.TabIndex = 3;
+            this.FileNameLbl.Text = "FileName:";
+            // 
+            // SudokuStatusStrip
+            // 
+            this.SudokuStatusStrip.Location = new System.Drawing.Point(0, 402);
+            this.SudokuStatusStrip.Name = "SudokuStatusStrip";
+            this.SudokuStatusStrip.Size = new System.Drawing.Size(935, 22);
+            this.SudokuStatusStrip.TabIndex = 4;
+            this.SudokuStatusStrip.Text = "statusStrip1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(432, 346);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(104, 28);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(542, 346);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(104, 28);
+            this.btnSaveAs.TabIndex = 6;
+            this.btnSaveAs.Text = "Save As...";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(322, 346);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(104, 28);
+            this.btnOpen.TabIndex = 7;
+            this.btnOpen.Text = "Open...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtFileNameDisplay
+            // 
+            this.txtFileNameDisplay.Location = new System.Drawing.Point(322, 305);
+            this.txtFileNameDisplay.Name = "txtFileNameDisplay";
+            this.txtFileNameDisplay.Size = new System.Drawing.Size(324, 20);
+            this.txtFileNameDisplay.TabIndex = 8;
+            // 
+            // lblLogInfo
+            // 
+            this.lblLogInfo.AutoSize = true;
+            this.lblLogInfo.Location = new System.Drawing.Point(12, 277);
+            this.lblLogInfo.Name = "lblLogInfo";
+            this.lblLogInfo.Size = new System.Drawing.Size(80, 13);
+            this.lblLogInfo.TabIndex = 9;
+            this.lblLogInfo.Text = "Logging Output";
+            // 
+            // lblTileTitle
+            // 
+            this.lblTileTitle.AutoSize = true;
+            this.lblTileTitle.Location = new System.Drawing.Point(327, 67);
+            this.lblTileTitle.Name = "lblTileTitle";
+            this.lblTileTitle.Size = new System.Drawing.Size(48, 13);
+            this.lblTileTitle.TabIndex = 10;
+            this.lblTileTitle.Text = "TileArray";
+            this.lblTileTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnGetTileForCell
+            // 
+            this.btnGetTileForCell.Location = new System.Drawing.Point(322, 97);
+            this.btnGetTileForCell.Name = "btnGetTileForCell";
+            this.btnGetTileForCell.Size = new System.Drawing.Size(140, 31);
+            this.btnGetTileForCell.TabIndex = 11;
+            this.btnGetTileForCell.Text = "Get Tile for selected Cell";
+            this.btnGetTileForCell.UseVisualStyleBackColor = true;
+            this.btnGetTileForCell.Click += new System.EventHandler(this.btnGetTileForCell_Click);
+            // 
+            // TileGridView
+            // 
+            this.TileGridView.AllowUserToAddRows = false;
+            this.TileGridView.AllowUserToDeleteRows = false;
+            this.TileGridView.AllowUserToResizeRows = false;
+            this.TileGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader;
+            this.TileGridView.BackgroundColor = System.Drawing.Color.Beige;
+            this.TileGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TileGridView.ColumnHeadersVisible = false;
+            this.TileGridView.Location = new System.Drawing.Point(322, 134);
+            this.TileGridView.Name = "TileGridView";
+            this.TileGridView.RowHeadersVisible = false;
+            this.TileGridView.RowHeadersWidth = 18;
+            this.TileGridView.Size = new System.Drawing.Size(140, 129);
+            this.TileGridView.TabIndex = 12;
+            // 
+            // lblLowestValue
+            // 
+            this.lblLowestValue.AutoSize = true;
+            this.lblLowestValue.Location = new System.Drawing.Point(484, 76);
+            this.lblLowestValue.Name = "lblLowestValue";
+            this.lblLowestValue.Size = new System.Drawing.Size(109, 13);
+            this.lblLowestValue.TabIndex = 13;
+            this.lblLowestValue.Text = "Lowest Value in Array";
+            // 
+            // txtLowestInArray
+            // 
+            this.txtLowestInArray.Location = new System.Drawing.Point(484, 134);
+            this.txtLowestInArray.Name = "txtLowestInArray";
+            this.txtLowestInArray.Size = new System.Drawing.Size(28, 20);
+            this.txtLowestInArray.TabIndex = 14;
+            // 
+            // btnGetLowestValue
+            // 
+            this.btnGetLowestValue.Location = new System.Drawing.Point(484, 99);
+            this.btnGetLowestValue.Name = "btnGetLowestValue";
+            this.btnGetLowestValue.Size = new System.Drawing.Size(129, 28);
+            this.btnGetLowestValue.TabIndex = 15;
+            this.btnGetLowestValue.Text = "Compute Lowest Value";
+            this.btnGetLowestValue.UseVisualStyleBackColor = true;
+            this.btnGetLowestValue.Click += new System.EventHandler(this.btnGetLowestValue_Click);
+            // 
+            // SudokuDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 424);
+            this.Controls.Add(this.btnGetLowestValue);
+            this.Controls.Add(this.txtLowestInArray);
+            this.Controls.Add(this.lblLowestValue);
+            this.Controls.Add(this.TileGridView);
+            this.Controls.Add(this.btnGetTileForCell);
+            this.Controls.Add(this.lblTileTitle);
+            this.Controls.Add(this.FileNameLbl);
+            this.Controls.Add(this.lblLogInfo);
+            this.Controls.Add(this.txtFileNameDisplay);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.SudokuStatusStrip);
+            this.Controls.Add(this.PreSuppliedGridView);
+            this.Controls.Add(this.txtLogDisplay);
+            this.Controls.Add(this.SudokuGridView);
+            this.Name = "SudokuDisplay";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SudokuGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PreSuppliedGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TileGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView SudokuGridView;
+        private System.Windows.Forms.RichTextBox txtLogDisplay;
+        private System.Windows.Forms.DataGridView PreSuppliedGridView;
+        private System.Windows.Forms.Label FileNameLbl;
+        private System.Windows.Forms.StatusStrip SudokuStatusStrip;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFileNameDisplay;
+        private System.Windows.Forms.Label lblLogInfo;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label lblTileTitle;
+        private System.Windows.Forms.Button btnGetTileForCell;
+        private System.Windows.Forms.DataGridView TileGridView;
+        private System.Windows.Forms.Label lblLowestValue;
+        private System.Windows.Forms.TextBox txtLowestInArray;
+        private System.Windows.Forms.Button btnGetLowestValue;
+    }
+}
+
---
+++ SudokuSolver/SudokuDisplay.cs
@@ -0,0 +1,440 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace SudokuSolver
+{
+    public partial class SudokuDisplay : Form
+    {
+        SudokuGrid SGrid;
+        Tile tile;
+
+        public SudokuDisplay()
+        {
+            InitializeComponent();
+
+            int[,] s = new int[9, 9];
+
+            //lets set up a grid with some values in 
+            s[0, 0] = 5;
+            s[0, 1] = 3;
+            s[0, 4] = 7;
+            s[1, 0] = 6;
+            s[1, 3] = 1;
+            s[1, 4] = 9;
+
+
+            SGrid = new SudokuGrid(s);
+
+            txtLogDisplay.Text = SGrid.ToString();
+
+            SGrid.UpdateDataGrid(SudokuGridView);
+
+        }
+
+        private void SudokuGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            int val;
+
+            if (Int32.TryParse(SudokuGridView.SelectedCells[0].Value.ToString(), out val) == true)
+            {
+                if (val > 0 && val <= 9)
+                {
+                    SGrid[e.RowIndex, e.ColumnIndex] = val;
+                    FileNameLbl.Text = "";
+                }
+                else
+                {
+                    SGrid[e.RowIndex, e.ColumnIndex] = 0;
+                    FileNameLbl.Text = "Please enter a number between 1 and 9";
+                }
+
+            }
+            else
+            {
+                SGrid[e.RowIndex, e.ColumnIndex] = 0;
+                FileNameLbl.Text = "Input must be an Integer bewteen 1 and 9";
+            }
+
+            SGrid.UpdateDataGrid(SudokuGridView);
+            SudokuGridView.EndEdit();
+
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string fileName;
+
+            fileName = txtFileNameDisplay.Text;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                SGrid.LoadGridFromFile(fileName);
+                SGrid.UpdateDataGrid(SudokuGridView);
+            }
+            else
+            {
+                fileName = "";
+            }
+
+            txtFileNameDisplay.Text = fileName;
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            string FileToSave;
+
+            saveFileDialog1.DefaultExt = "*.sdg";
+            saveFileDialog1.Filter = "Sudoku Grid File(*.sdg)|*.sdg";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileToSave = saveFileDialog1.FileName;
+                SGrid.SaveGridToFile(FileToSave);
+            }
+            else
+            {
+                FileToSave = "";
+            }
+
+            txtFileNameDisplay.Text = FileToSave;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetTileForCell_Click(object sender, EventArgs e)
+        {
+            // get selected Cell and then create an Tile and then Display
+            int[,] myTileArray = SGrid.GetTileArrayForCell(SudokuGridView.CurrentCell.RowIndex, SudokuGridView.CurrentCell.ColumnIndex);
+
+            tile = SGrid.GetTileForCell(SudokuGridView.CurrentCell.RowIndex, SudokuGridView.CurrentCell.ColumnIndex);
+
+            tile.UpdateDataGrid(TileGridView);
+
+        }
+
+        private void btnGetLowestValue_Click(object sender, EventArgs e)
+        {
+            int lowestNum = tile.MinimumValidNumber();
+        }
+    }
+
+    public class SudokuGrid
+    {
+        private int[,] myGrid;
+        private bool[,] PreSuppliedValue;
+
+        public int this[int row, int column]
+        {
+            get
+            {
+                return myGrid[row, column];
+            }
+            set
+            {
+                myGrid[row, column] = value;
+            }
+        }
+
+
+        public SudokuGrid(int[,] InitialValues)
+        {
+            myGrid = InitialValues;
+            PreSuppliedValue = new bool[InitialValues.GetLength(0), InitialValues.GetLength(1)];
+
+            // find the non 0 values and then set the PreSuppliedValue to True for them 
+            for (int rowCount = 0; rowCount < InitialValues.GetLength(0); rowCount++)
+            {
+                for (int colCount = 0; colCount < InitialValues.GetLength(1); colCount++)
+                {
+                    PreSuppliedValue[rowCount, colCount] = InitialValues[rowCount, colCount] > 0;
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            int width, height;
+
+            height = myGrid.GetUpperBound(0) + 1;
+            width = myGrid.GetUpperBound(1) + 1;
+
+            sb.AppendLine();
+
+            for (int i = 0; i <= myGrid.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= myGrid.GetUpperBound(1); j++)
+                {
+                    sb.Append(this[i, j] + " ");
+                }
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+
+        public void UpdateDataGrid(DataGridView myDGV)
+        {
+            var rowCount = myGrid.GetLength(0);
+            var rowLength = myGrid.GetLength(1);
+
+            myDGV.Rows.Clear();
+
+            myDGV.ColumnCount = rowLength;
+
+            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+            {
+                var row = new DataGridViewRow();
+
+                for (int columnIndex = 0; columnIndex < rowLength; ++columnIndex)
+                {
+                    var tmp = "";
+                    var cell = new DataGridViewTextBoxCell();
+
+                    if (myGrid[rowIndex, columnIndex] != 0)
+                    {
+                        tmp = myGrid[rowIndex, columnIndex].ToString();
+                    }
+                    else
+                    {
+                        tmp = "";
+                    }
+                    cell.Value = tmp;
+                    row.Cells.Add(cell);
+                }
+                myDGV.Rows.Add(row);
+            }
+        }
+
+        public void SaveGridToFile(string FileName)
+        {
+            FileStream stream = File.Create(FileName);
+            var formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, myGrid);
+            stream.Close();
+        }
+
+        public void LoadGridFromFile(string FileName)
+        {
+
+            // Restore from file
+            FileStream stream = File.OpenRead(FileName);
+            var formatter = new BinaryFormatter();
+            myGrid = (int[,])formatter.Deserialize(stream);
+            stream.Close();
+
+        }
+
+        public Tile GetTileForCell(int row, int column)
+        {
+
+            int RowsInTile = myGrid.GetUpperBound(0) / 3;
+            int ColsInTile = myGrid.GetUpperBound(1) / 3;
+
+            int[,] TileAsArray = GetTileArrayForCell(row, column);
+
+
+
+            return new Tile(TileAsArray);
+
+
+
+        }
+
+        public int[,] GetTileArrayForCell(int row, int column)
+        {
+
+            // a Sudoku Grid may have dimensions other than 9 by 9 but it will always be made up of 9 "tiles"
+            // in the scope of this current implementation.  I just don't have the patience or the will for all 
+            // the weird version of the puzzle just now. 
+            int RowsInTile = myGrid.GetUpperBound(0) / 3;
+            int ColsInTile = myGrid.GetUpperBound(1) / 3;
+
+
+            int[,] TileAsArray = new int[3, 3];
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    TileAsArray[i, j] = myGrid[row - (row % 3) + i, column - (column % 3) + j];
+                }
+            }
+
+
+            return TileAsArray;
+
+        }
+
+    }
+
+
+    public class SudokuBruteForce : SudokuGrid
+    {
+
+        public SudokuBruteForce(int[,] s) : base(s) { }
+
+        public void Solve()
+        {
+            // look at the internal grid and see 
+
+        }
+
+
+
+    }
+
+    public class Tile
+    {
+
+        private int[,] myTile;
+        private bool[,] PreSuppliedValue;
+
+        public Tile(int[,] InitialValues)
+        {
+            myTile = InitialValues;
+            PreSuppliedValue = new bool[InitialValues.GetLength(0), InitialValues.GetLength(1)];
+
+            // find the non 0 values and then set the PreSuppliedValue to True for them 
+            for (int rowCount = 0; rowCount < InitialValues.GetLength(0); rowCount++)
+            {
+                for (int colCount = 0; colCount < InitialValues.GetLength(1); colCount++)
+                {
+                    PreSuppliedValue[rowCount, colCount] = InitialValues[rowCount, colCount] > 0;
+                }
+            }
+
+        }
+
+        public Tile()
+        {
+
+        }
+
+        public int[,] GetArray()
+        {
+            return myTile;
+        }
+
+        public int this[int row, int column]
+        {
+            get
+            {
+                return myTile[row, column];
+            }
+            set
+            {
+                myTile[row, column] = value;
+            }
+        }
+
+        public int MinimumValidNumber()
+        {
+            List<int> myList = new List<int>();
+            int[,] myArray;
+
+            myList.AddRange(GetStartingNumbers());
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (myTile[i, j] != 0)
+                    {
+                        myList.Remove(myTile[i, j]);
+                    }
+                }
+            }
+
+            return myList.Min();
+        }
+
+        public int[] GetStartingNumbers()
+        {
+            return new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        }
+
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            int width, height;
+
+            height = myTile.GetUpperBound(0) + 1;
+            width = myTile.GetUpperBound(1) + 1;
+
+            sb.AppendLine();
+
+            for (int i = 0; i <= myTile.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= myTile.GetUpperBound(1); j++)
+                {
+                    sb.Append(this[i, j] + " ");
+                }
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+
+
+        public void UpdateDataGrid(DataGridView myDGV)
+        {
+            var rowCount = myTile.GetLength(0);
+            var rowLength = myTile.GetLength(1);
+
+            myDGV.Rows.Clear();
+
+            myDGV.ColumnCount = rowLength;
+
+            for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex)
+            {
+                var row = new DataGridViewRow();
+
+                for (int columnIndex = 0; columnIndex < rowLength; ++columnIndex)
+                {
+                    var tmp = "";
+                    var cell = new DataGridViewTextBoxCell();
+
+                    if (myTile[rowIndex, columnIndex] != 0)
+                    {
+                        tmp = myTile[rowIndex, columnIndex].ToString();
+                    }
+                    else
+                    {
+                        tmp = "";
+                    }
+                    cell.Value = tmp;
+                    row.Cells.Add(cell);
+                }
+                myDGV.Rows.Add(row);
+            }
+        }
+
+    }
+
+}
